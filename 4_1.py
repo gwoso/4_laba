9852bbe5a65a04e0be605fc0113ae3838e2e204c
Ksenia <gwoso@yandex.ru>
2024-03-09 00:01:30 +0500

    func zamikania

--- 4_1.py
+++ 4_1.py
@@ -1,3 +1,22 @@
 #Замыкание для отслеживания количества HP героя - HP не может 
 #подниматься больше 100 и опускаться ниже 0, герой может лечиться 
 #или получать урон.
+
+def hero():
+    hp = 100
+
+    def heal(x):
+        nonlocal hp 
+        hp = min(hp + x, 100)
+        return hp
+    
+    def damage(x):
+        nonlocal hp
+        hp = max(hp - x, 0)
+        return hp
+    
+    return heal, damage
+
+heal, damage = hero()
+print(damage(30))
+print(heal(10))
