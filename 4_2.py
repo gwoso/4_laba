166b7dd3b388ce86fc4e2541a96151bc3a29345f
Ksenia <150545779+gwoso@users.noreply.github.com>
2024-03-09 20:29:09 +0500

    Update 4_2.py

--- 4_2.py
+++ 4_2.py
@@ -1,1 +1,13 @@
+#Декоратор для подавления вывода функции на консоль.
 
+def decorator(func):
+    def wrapper(*args, **kwargs):
+        return
+    return wrapper
+
+@decorator
+def simple_func():
+    x = 2 + 3
+    return x
+
+print(simple_func())
