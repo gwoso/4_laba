a03588b6f129f01871b665f845285f168b74f7f9
Ksenia <150545779+gwoso@users.noreply.github.com>
2024-03-09 19:57:14 +0500

    Update 4_1.py

--- 4_1.py
+++ 4_1.py
@@ -2,21 +2,30 @@
 #подниматься больше 100 и опускаться ниже 0, герой может лечиться 
 #или получать урон.
 
+
+def decorator(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        return min(max(result, 0), 100)
+    return wrapper
+
 def hero():
     hp = 100
 
+    @decorator
     def heal(x):
         nonlocal hp 
-        hp = min(hp + x, 100)
+        hp += x      #hp = min(hp + x, 100)
         return hp
     
+    @decorator
     def damage(x):
         nonlocal hp
-        hp = max(hp - x, 0)
+        hp -= x      #hp = max(hp - x, 0)
         return hp
     
     return heal, damage
 
 heal, damage = hero()
 print(damage(30))
-print(heal(10))
+print(heal(40))
